54ce9913644ec17f7759156fcf64ec2398f4b78a
onurbasdas <onurebasdase@hotmail.com>
2021-02-06 16:54:01 +0300

    first commit

---
+++ ListApp/Game.swift
@@ -0,0 +1,8 @@
+//
+//  Game.swift
+//  ListApp
+//
+//  Created by Onur Başdaş on 6.02.2021.
+//
+
+import Foundation
---
+++ ListApp/GameStore.swift
@@ -0,0 +1,8 @@
+//
+//  GameStore.swift
+//  ListApp
+//
+//  Created by Onur Başdaş on 6.02.2021.
+//
+
+import Foundation
