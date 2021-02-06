78bc7771158709b4d4fddbae24b51c893c83ce7f
onurbasdas <onurebasdase@hotmail.com>
2021-02-06 16:08:43 +0300

    Initial Commit

---
+++ ListApp/ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppApp.swift
+//  ListApp
+//
+//  Created by Onur Başdaş on 6.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
